b639777bc0182d24b3b9e9fecbc9c3e60eb90501
hduser <1365519162@qq.com>
2022-01-23 17:11:36 +0800

    ADD:autoShell

---
+++ autoShell.sh
@@ -0,0 +1,15 @@
+python3 genMapTable.py
+cd ./Logger
+python3 main.py &
+sleep 10
+echo "run logSyatem"
+cd ../logSystem
+python3 logSystem.py
+
+for port in  $(seq 5000 5400)
+do
+	pid=$(lsof -i:$port | awk '{print $2}'| awk 'NR==2')
+	if [  -n  "$pid"  ];  then
+		sudo kill  -9  $pid;
+	fi
+done
